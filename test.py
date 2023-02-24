ef3ccaa966fbefc4743e24843e56cbf81e13df90
Tommy Graves <tommy@rwx.com>
2023-02-23 22:26:39 -0500

    testing retries

--- test.py
+++ test.py
@@ -1,9 +1,19 @@
 import unittest
 import xmlrunner
+import random
 
 class TestFunctions(unittest.TestCase):
     def test_truth(self):
         self.assertTrue(True)
+    def test_the_falsity(self):
+        self.assertTrue(False)
+    def test_the_falsity_2(self):
+        self.assertTrue(False)
+    def test_flaky(self):
+        if random.randint(0,9) > 4:
+            self.assertTrue(True)
+        else:
+            self.assertTrue(False)
 
 if __name__ == '__main__':
     unittest.main(
